48eb0ecef10dfacb11ab4368b2264551036870f0
huxinjie123 <2539323471@qq.com@example.com>
2020-11-02 16:39:55 +0800

    homework

--- src/main/java/com/xjhu/study/homework/week5/BookServiceImpl.java
+++ src/main/java/com/xjhu/study/homework/week5/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.xjhu.study.homework.week5;
 
+import java.util.regex.Pattern;
+
 /**
  * @author huxinjie
  * @date 2020/11/2 15:46
@@ -8,8 +10,8 @@ public class BookServiceImpl implements BookService {
     @Override
     public boolean BookUpload(Book book) throws MyException {
         boolean b = false;
-        if (book.getISBN().length() != 13){
-            throw new MyException("图书编号必须为13位！");
+        if (book.getISBN().length() != 13 || !Pattern.compile("[0-9]*").matcher(book.getISBN()).matches()){
+            throw new MyException("图书编号必须为13位数字！");
         }else if (book.getName().contains("暴力")){
             throw new MyException("图书名称含有禁忌文字！！");
         }
--- src/main/java/com/xjhu/study/homework/week5/TestMyException.java
+++ src/main/java/com/xjhu/study/homework/week5/TestMyException.java
@@ -7,13 +7,13 @@ package com.xjhu.study.homework.week5;
 public class TestMyException {
     public static void main(String[] args) {
         Book book1 = new Book("1234567891357","暴力美学",30);
-        Book book2 = new Book("1234567892468","健美力学",130);
+        Book book2 = new Book("123456789246a","健美力学",130);
         Book book3 = new Book("123456","健美",25);
         Book book4 = new Book("1234567892468","健美力学",30);
         BookService bookService = new BookServiceImpl();
         boolean result = false;
         try {
-            result = bookService.BookUpload(book4);
+            result = bookService.BookUpload(book3);
         }catch (MyException e){
             System.err.println(e.toString());
         }finally {
